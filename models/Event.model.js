f192d18374b7204dd8b8a94a6bbe999e2fd7d740
Anna Kazulak <anna.kazulak@gmail.com>
2023-09-20 16:28:37 +0200

    add event model

---
+++ models/Event.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const eventSchema = new Schema(
+  {
+    title: { type: String, required: [true, "Event title is required"] },
+    desciption: String,
+    date: Date,
+    time: String,
+    location: String,
+    imageUrl: { type: String, required: false },
+    guests: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Guest",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("event", eventSchema);
