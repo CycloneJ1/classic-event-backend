8badfaf21b441de715cdaa6413d40bf351dd04c3
Anna Kazulak <anna.kazulak@gmail.com>
2023-09-22 13:17:26 +0200

    add claudinary and multer, add route for file upload

--- routes/guest.routes.js
+++ routes/guest.routes.js
@@ -4,6 +4,23 @@ const mongoose = require("mongoose");
 
 const Event = require("../models/Event.model");
 const Guest = require("../models/Guest.model");
+const fileUploader = require("../config/cloudinary.config");
+
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+  console.log("file is: ", req.file)
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  
+  res.json({ fileUrl: req.file.path });
+});
 
 router.post("/guests", (req, res, next) => {
   const { name, description, imageUrl, eventId } = req.body;
